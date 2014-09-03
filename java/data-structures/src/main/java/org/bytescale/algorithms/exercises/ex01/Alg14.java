f90212bd218f915ef64e700886728f6cfd6479b7
Jared Holmberg <jared.holmberg@gmail.com>
2014-09-02 20:45:40 -0400

    Moved all exercises from chapter one into the same package.

---
+++ java/data-structures/src/main/java/org/bytescale/algorithms/exercises/ex01/Alg14.java
@@ -0,0 +1,18 @@
+package org.bytescale.algorithms.exercises.ex01;
+
+/**
+ * What is wrong, if anything, with these statements?
+ */
+public class Alg14 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+//        if (a > b) then c = 0;  //then is not a keyword, compiler error
+//        if a > b { c = 0; } //missing parens around condition, compiler error
+//        if (a > b) c = 0; //valid
+//        if (a > b) c = 0 else b = 0; //missing semicolon after c=0
+
+    }
+}
---
+++ java/data-structures/src/main/java/org/bytescale/algorithms/exercises/ex01/Alg15.java
@@ -0,0 +1,36 @@
+package org.bytescale.algorithms.exercises.ex01;
+
+import java.util.Scanner;
+
+/**
+ * Write a code fragment that prints true if the double variables x and y are both strictly
+ * between 0 and 1 and false otherwise.
+ * Sedgewick, Robert; Wayne, Kevin (2011-02-21). Algorithms (4th Edition) (Kindle Locations 1276-1278). Pearson Education. Kindle Edition.
+ */
+public class Alg15 {
+    public static void main(String[] args) {
+        System.out.println("Enter 2 doubles and the program will tell you if they are strictly between 0 and 1.");
+        String newline = System.getProperty("line.separator");
+        double x = 0;
+        double y = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please provide a double value for x: ");
+        x = in.nextDouble();
+        System.out.print(newline);
+        System.out.print("Please provide a double value for y: ");
+        y = in.nextDouble();
+        System.out.print(newline);
+        if (isInRange(x, 0, 1) && isInRange(y, 0, 1)) {
+            System.out.println("true");
+        }  else {
+            System.out.println("false");
+        }
+
+
+
+    }
+
+    public static boolean isInRange(double val, double lowerBound, double upperBound) {
+        return (lowerBound < val && val < upperBound);
+    }
+}
