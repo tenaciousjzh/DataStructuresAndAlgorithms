9439cb2340c22282f2417c55ace71b1a008c25d9
Jared Holmberg <jared.holmberg@gmail.com>
2014-09-02 22:16:03 -0400

    Added lg() algorithm in Exercise 14

--- java/data-structures/src/main/java/org/bytescale/algorithms/exercises/ex01/Alg14.java
+++ java/data-structures/src/main/java/org/bytescale/algorithms/exercises/ex01/Alg14.java
@@ -1,18 +1,39 @@
 package org.bytescale.algorithms.exercises.ex01;
 
 /**
- * What is wrong, if anything, with these statements?
+ * Write a static method lg() that takes an int value N as argument and returns the largest int 
+ * not larger than the base-2 logarithm of N. Do not use Math.
+ * 
+ * Remember: log (8) = 3
+ *              2
+ * is the same as 2 * 2 * 2 = 8
+ *                |---3---| 
+ * 2 is the number being multiplied (or the base)
+ * 
+ * 
  */
 public class Alg14 {
-    public static void main(String[] args) {
-        int a = 0;
-        int b = 0;
-        int c = 0;
-
-//        if (a > b) then c = 0;  //then is not a keyword, compiler error
-//        if a > b { c = 0; } //missing parens around condition, compiler error
-//        if (a > b) c = 0; //valid
-//        if (a > b) c = 0 else b = 0; //missing semicolon after c=0
-
-    }
+	/**
+	 * Returns the largest int not larger than the base-2 logarithm of N
+	 * @param N
+	 * @return
+	 */
+	public static int lg(int N) {
+		if (N < 0) throw new IllegalArgumentException("Must supply a positive integer greater than 0");
+		
+		int n = N;
+		int log = 0;
+		int base = 2;
+		while (n >= base) {
+			n = n / base;
+			log++;
+		}
+		return log;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("lg(8) = the largest int not greater than the base 2 logarithm of the number N supplied as the argument to the lg() function.");
+		System.out.println("lg(8) = " + lg(8));
+		System.out.println("lg(7) = " + lg(7));
+	}
 }
