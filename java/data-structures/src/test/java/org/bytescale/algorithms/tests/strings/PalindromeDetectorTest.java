a4d1174e976031b0baba5e45ca6af153de41b1d9
Jared Holmberg <jared.holmberg@gmail.com>
2014-09-03 21:28:56 -0400

    updated tests for palindrome

--- java/data-structures/src/test/java/org/bytescale/algorithms/tests/strings/PalindromeDetectorTest.java
+++ java/data-structures/src/test/java/org/bytescale/algorithms/tests/strings/PalindromeDetectorTest.java
@@ -3,7 +3,7 @@ package org.bytescale.algorithms.tests.strings;
 import org.junit.Test;
 
 import static org.bytescale.algorithms.strings.PalindromeDetector.isPalindrome;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class PalindromeDetectorTest {
@@ -14,6 +14,8 @@ public class PalindromeDetectorTest {
 		assertTrue(isPalindrome(palindrome));
 		String evenLengthPalindrome = "abba";
 		assertTrue(isPalindrome(evenLengthPalindrome));
+		String nonPalindrome = "evil";
+		assertFalse(isPalindrome(nonPalindrome));
 	}
 	
 }
