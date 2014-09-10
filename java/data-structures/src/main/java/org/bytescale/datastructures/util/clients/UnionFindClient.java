0028d461e3ab818774565e554ec6354cd9f629e5
Jared Holmberg <jared.holmberg@gmail.com>
2014-09-10 05:09:57 -0400

    Built client to test the different UnionFind implementations

---
+++ java/data-structures/src/main/java/org/bytescale/datastructures/util/clients/UnionFindClient.java
@@ -0,0 +1,32 @@
+package org.bytescale.datastructures.util.clients;
+
+import org.bytescale.datastructures.spec.linked.UnionFind;
+import org.bytescale.datastructures.util.In;
+import org.bytescale.datastructures.util.StdOut;
+import org.bytescale.datastructures.util.linked.BasicUnionFind;
+import org.bytescale.datastructures.util.linked.WeightedQuickUnion;
+import org.bytescale.datastructures.util.linked.WeightedQuickUnionWithPathCompression;
+
+public class UnionFindClient {
+
+	public static void main(String[] args) {
+		In input = new In(args[0]);
+		int N = input.readInt();
+		UnionFind uf = new WeightedQuickUnionWithPathCompression(N);
+		while (input.hasNextLine()) {
+			String line = input.readLine();
+			if (line == null || line.equals("")) {
+				continue;
+			}
+			String[] rawInputs = line.split(" ");
+			int p = Integer.parseInt(rawInputs[0]);
+			int q = Integer.parseInt(rawInputs[1]);
+			if (uf.connected(p, q)) {
+				continue;
+			}
+			uf.union(p, q);
+			StdOut.println(p + " " + q);
+		}
+		StdOut.println("Found " + uf.count() + " component groups of connected objects.");
+	}
+}
